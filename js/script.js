71af42e6786f353ae81c530f2185c9e80a46273e
zampiello alexia <alexzamp@hotmail.fr>
2018-07-26 12:18:01 +0200

    mise en place verbe et temps

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 console.log('>>>script.js');
+
+$(document).ready(function() {
+
+
+      var htmlcard = `<div>
+    <div class="uk-light uk-background-secondary  uk-card uk-card-default">
+        <div class="uk-card-media-top">
+            <img src="img/xefsotnveui9fb6wkak2.jpg" alt="">
+        </div>
+        <div class="uk-card-body">
+            <h3 class="uk-card-title">to eat</h3>
+            <p>Present perfect</p>
+
+        </div>
+    </div>
+</div>`
+
+
+var tenses = ["Present perfect", "futur", "Preterite", "past Continuous", "past perfect", "présent simple"];
+
+var verbs =
+});
